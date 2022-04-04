46b33154fd9d62fe7dc601ab95487a3223be20f4
nabilaathiyah <101244178+nabilaathiyah@users.noreply.github.com>
2022-04-04 23:24:58 +0700

    web_CRUD

--- student/src/main/java/id/ac/uin/student/student/entity/Student.java
+++ student/src/main/java/id/ac/uin/student/student/entity/Student.java
@@ -13,21 +13,25 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
-    @Column(name = "first_name")
+    @NotEmpty (message = "First name is required")
+    @Column(nullable = false, name = "first_name")
     private String firstname;
 
-    @NotEmpty
-    @Column(name = "last_name")
+    @NotEmpty(message = "Last name is required")
+    @Column(nullable = false, name = "last_name")
     private String lastname;
 
-    @NotEmpty
-    @Column(name = "email", unique = true)
+    @NotEmpty(message = "email is required")
+    @Column(nullable = false, name = "email", unique = true)
     private String email;
 
-    @NotEmpty
-    @Column(name="phone_number", unique = true)
+    @NotEmpty(message = "Phone number is required")
+    @Column(nullable = false, unique = true)
     private String phoneNumber;
+
+    @NotEmpty(message = "Jurusan is required")
+    @Column(nullable = false, unique = true)
+    private String jurusan;
 
 
 }
